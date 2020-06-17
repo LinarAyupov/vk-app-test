dc69f19ad3dd619c6a8e6c020ead92103049206b
Your Name <you@example.com>
2020-06-18 02:35:30 +0600

    Updates

---
+++ precache-manifest.8d22f8821785c102f80cf31dbfb68590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c72202731f0a5b13c5a943d6efe632",
+    "url": "/vk-app-test/index.html"
+  },
+  {
+    "revision": "376be113fe3737c306d8",
+    "url": "/vk-app-test/static/css/main.cdf34c77.chunk.css"
+  },
+  {
+    "revision": "53ffb335a37c7f56406d",
+    "url": "/vk-app-test/static/js/2.1870a9e9.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/vk-app-test/static/js/2.1870a9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376be113fe3737c306d8",
+    "url": "/vk-app-test/static/js/main.b106f06a.chunk.js"
+  },
+  {
+    "revision": "f792db4c3ec80881cd53",
+    "url": "/vk-app-test/static/js/runtime-main.8506fa3d.js"
+  }
+]);
