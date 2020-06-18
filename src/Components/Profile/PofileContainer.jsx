8e7cd6bc4bd9c9376e1a12d0a33073ef17c8ebb9
Your Name <you@example.com>
2020-06-18 18:53:31 +0600

    asca

--- src/Components/Profile/PofileContainer.jsx
+++ src/Components/Profile/PofileContainer.jsx
@@ -9,7 +9,7 @@ import FriendsListContainer from "../Friends/FriendsListContainer";
 class ProfileContainer extends React.Component {
 
     showFriends = () => {
-        return axios.jsonp(`https://api.vk.com/method/friends.search?user_id=${this.props.profile.id}&fields=photo_50&count=1000&access_token=3667a9113667a9113667a911b836150a1a336673667a911688f6547e73321d48e3f1db1&v=5.52`)
+        return axios.jsonp(`https://api.vk.com/method/friends.get?user_id=${this.props.profile.id}&fields=photo_50&count=1000&access_token=3667a9113667a9113667a911b836150a1a336673667a911688f6547e73321d48e3f1db1&v=5.52`)
             .then(
                 response => {
                     if (response.response === undefined) {
